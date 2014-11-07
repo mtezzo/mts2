88e2bd75f5bb8311df51f57075da8ef3e8840889
jaredtritz <jaredtritz@gmail.com>
2014-11-07 17:18:02 -0500

    updates to get the message rendering

--- Utilities/Tool Support/application.py
+++ Utilities/Tool Support/application.py
@@ -2,3 +2,11 @@
 # Prefix functions with @expose to be able to use them in derived calculations.
 
 from tailoring2.extensions import *
+
+@toplevel_sources
+def tlses(treq):
+    return ["", "Source1"]
+    #return ["", "Source1", "Common1"]
+    #return ["", "Roster", "Course_Setup", "Student_Survey", "Gradebook", "SLC"]
+
+
